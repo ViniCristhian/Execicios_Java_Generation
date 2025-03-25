298bae34f0fceea8c9d91fad2ca4e09322324dc5
ViniCristhian <viniciuscristhian34@gmail.com>
2025-03-25 18:19:52 -0300

    Entraga da atividade prativa Java 9 Estrutura de dados fila e pilha

---
+++ src/java_prat_9/Atividade1_Fila.java
@@ -0,0 +1,125 @@
+package java_prat_9;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Atividade1_Fila {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Escreva um programa contendo uma Collection Queue (Fila) de Objetos do tipo
+		 * string, para organizar uma fila por ordem de chegada dos Clientes de um
+		 * Banco. O programa deverá ter um Menu que aceitará as opções 0, 1, 2 e 3: 
+		 * -- 1: Adicionar um novo Cliente na fila. Deve solicitar o nome do Cliente. 
+		 * -- 2: Listar todos os Clientes na fila 
+		 * -- 3: Chamar (retirar) uma pessoa da fila 
+		 * -- 0: O programa deve ser finalizado.
+		 * Caso a fila esteja vazia, o programa deverá informar que a fila está vazia ao
+		 * tentar retirar (chamar) um cliente da fila.
+		 */
+
+		Scanner scanner = new Scanner(System.in);
+
+		Queue<String> fila = new LinkedList<String>();
+		
+		int opcaoDesejada;
+		
+		boolean finalizarPrograma = true;
+		
+		while (finalizarPrograma) {
+			
+			System.out.println("\n*****************************************************************************");
+			
+			System.out.println("\n1: Adicionar um novo Cliente na fila.");
+			System.out.println("2: Listar todos os Clientes na fila");
+			System.out.println("3: Chamar uma pessoa da fila");
+			System.out.println("0: O programa deve ser finalizado.");
+			
+			
+			System.out.println("\n*****************************************************************************");
+			
+			System.out.print("Entre com a opção desejada: ");
+			opcaoDesejada = scanner.nextInt();
+			
+			if (opcaoDesejada == 1) {
+				adicionarPessaoAFila(fila, scanner);
+			} 
+
+			if (opcaoDesejada == 2) {
+				listarClientes(fila);
+			}
+			
+			if (opcaoDesejada == 3) {
+				chamarCliente(fila);
+			}
+			
+			if (opcaoDesejada == 0) {
+				finalizarPrograma = false;
+			}
+			
+			if (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 0) {
+				System.out.println("Opção desejada não compativel com o sistema");
+			}
+		}
+		
+		scanner.close();
+	}
+	
+	private static void adicionarPessaoAFila(Collection<String> fila, Scanner scanner) {
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+		
+		System.out.print("\nDigite o nome do Cliente: ");
+		scanner.nextLine();
+		
+		fila.add(scanner.nextLine());
+		
+		System.out.println("\nCliente adicionado com sucesso!");
+	}
+	
+	private static void listarClientes(Collection<String> fila) {
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+		
+		if (fila.isEmpty()) {
+			System.out.println("A Fila está vazia");
+			return;
+		}
+		
+		System.out.println("\nLista de clientes na fila: ");
+		
+		for (String cliente : fila) {
+			System.out.println(cliente);
+		}
+		
+		
+	}
+	
+	private static void chamarCliente(Queue<String> fila) {
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+		
+		if (fila.isEmpty()) {
+			System.out.println("A fila está vazia!");
+			return;
+		}
+
+		System.out.println("Cliente chamado: " + fila.poll());
+		
+		System.out.println("\nClientes restantes na fila: ");
+		
+		for (String cliente : fila) {
+			System.out.println(cliente);
+		}
+	}
+	
+}
---
+++ src/java_prat_9/Atividade2_Pilha.java
@@ -0,0 +1,123 @@
+package java_prat_9;
+
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Atividade2_Pilha {
+
+	public static void main(String[] args) {
+		
+		/* Escreva um programa contendo uma Collection Stack (Pilha) de Objetos string, para organizar
+		 * a retirada de livros de uma pilha. O programa deverá ter um Menu que aceitará as opções 0, 1, 2 e 3:
+		 * -- 1: Adicionar um novo livro na pilha. Deve solicitar o nome do livro.
+		 * -- 2: Listar todos os livros da Pilha
+		 * -- 3: Retirar um livro da pilha 
+		 * -- 0: O programa deve ser finalizado. 
+		 *	Caso a pilha esteja vazia, ao tentar retirar um livro da pilha, o programa deverá informar 
+		 *	que a pilha está vazia.
+		 */
+
+		
+		Scanner scanner = new Scanner(System.in);
+
+		Stack<String> pilha = new Stack<String>();
+		
+		int opcaoDesejada;
+		
+		boolean finalizarPrograma = true;
+		
+		while (finalizarPrograma) {
+			
+			System.out.println("\n*****************************************************************************");
+			
+			System.out.println("\n1: Adicionar um novo livro na pilha");
+			System.out.println("2: Listar todos os livros da Pilha");
+			System.out.println("3: Retirar um livro da pilha");
+			System.out.println("0: O programa deve ser finalizado.\n");
+			
+			
+			System.out.println("*****************************************************************************");
+			
+			System.out.print("Entre com a opção desejada: ");
+			opcaoDesejada = scanner.nextInt();
+			
+			if (opcaoDesejada == 1) {
+				adicionarLivroAPilha(pilha, scanner);
+			} 
+
+			if (opcaoDesejada == 2) {
+				listarLivros(pilha);
+			}
+			
+			if (opcaoDesejada == 3) {
+				retirarLivro(pilha);
+			}
+			
+			if (opcaoDesejada == 0) {
+				finalizarPrograma = false;
+				
+			}
+			
+			if (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 0) {
+				System.out.println("\nOpção desejada não compativel com o sistema");
+			}
+		}
+		
+		scanner.close();
+
+	}
+	
+	private static void adicionarLivroAPilha(Stack<String> pilha, Scanner scanner) {
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+		
+		System.out.print("\nDigite o nome do Livro: ");
+		scanner.nextLine();
+		
+		pilha.push(scanner.nextLine());
+		
+		System.out.println("\nLivro adicionado com sucesso!");
+	}
+
+	private static void listarLivros(Collection<String> pilha) {
+
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+
+		if (pilha.isEmpty()) {
+			System.out.println("A Pilha está vazia");
+			return;
+		}
+
+		System.out.println("\nLista de livros na pilha: ");
+
+		for (String cliente : pilha) {
+			System.out.println(cliente);
+		}
+
+	}
+
+	private static void retirarLivro(Stack<String> pilha) {
+
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+
+		if (pilha.isEmpty()) {
+			System.out.println("A Pilha está vazia");
+			return;
+		}
+
+		System.out.println("Livro retirado: " + pilha.pop());
+
+		System.out.println("\nLivros restantes na pilha: ");
+
+		for (String cliente : pilha) {
+			System.out.println(cliente);
+		}
+	}
+}
